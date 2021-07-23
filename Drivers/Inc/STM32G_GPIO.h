0f3d56671a1e53f4487e8c72a0715fd157fd9eb2
Romanandhan <romanandhan@gmail.com>
2021-07-23 23:16:20 +0530

    create .c and .h files for GPIO Driver

---
+++ Drivers/Inc/STM32G_GPIO.h
@@ -0,0 +1,14 @@
+/*
+ * STM32G_GPIO.h
+ *
+ *  Created on: 23-Jul-2021
+ *      Author: romanandhan l
+ */
+
+#ifndef INC_STM32G_GPIO_H_
+#define INC_STM32G_GPIO_H_
+
+#include "STM32G070.h"
+
+
+#endif /* INC_STM32G_GPIO_H_ */
---
+++ Drivers/Src/STM32G_GPIO.c
@@ -0,0 +1,8 @@
+/*
+ * STM32G_GPIO.c
+ *
+ *  Created on: 23-Jul-2021
+ *      Author: romanandhan l
+ */
+
+#include "STM32G_GPIO.h"
