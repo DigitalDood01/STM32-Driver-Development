aab27d092542e74d477459895e3ce721fef2975b
Romanandhan <romanandhan@gmail.com>
2021-08-18 18:55:54 +0530

    Configure the SPI_Init API

--- Drivers/Src/STM32G_SPI.c
+++ Drivers/Src/STM32G_SPI.c
@@ -14,19 +14,66 @@
  *
  * Function Name 				- SPI_Init
  *
- * Brief 						-
+ * Brief 						- This API initializes the SPI_CR1 register based on the programmer's input
  *
- * Param1						-
+ * Param1						- pointer for the SPI handle structure
  * Param2						-
  * Param3 						-
  *
- * Return 						-
+ * Return 						- None
  *
  * Note 						-
  ************************************************************************************************************************************/
 
-void SPI_Init(SPI_Handle_t *pSPI_Handle);
+void SPI_Init(SPI_Handle_t *pSPI_Handle)
+{
+	/* Configure the SPI1_CR1 Register */
 
+	/* 1. Configure the device mode */
+	uint32_t tempreg = 0;
+	tempreg |= pSPI_Handle->SPI_Config.SPI_DeviceMode << SPI_CR1_MSTR;
+
+	/* 2 Configure the bus configuration */
+	if(pSPI_Handle->SPI_Config.SPI_BusConfig == SPI_BUS_CONFIG_FD)
+	{
+		/* BIDI bit in SPI_CR1 Register should be cleared */
+		tempreg &= ~(1 << SPI_CR1_BIDIMODE);
+	}
+
+	else if(pSPI_Handle->SPI_Config.SPI_BusConfig == SPI_BUS_CONFIG_HD)
+	{
+		/* BIDI bit in SPI_CR1 Register should be set */
+		tempreg |= (1 << SPI_CR1_BIDIMODE);
+	}
+
+	else if(pSPI_Handle->SPI_Config.SPI_BusConfig == SPI_BUS_CONFIG_SIMPLEX_RXONLY)
+	{
+		/* BIDI bit in SPI_CR1 Register should be cleared */
+
+		tempreg &= ~(1 << SPI_CR1_BIDIMODE);
+
+		/* RXONLY bit in SPI_CR1 Register should be set */
+		tempreg |= (1 << SPI_CR1_RXONLY);
+	}
+
+	/* 3. Configure the data length format(whether it is 8 bit or 16 bit) */
+	tempreg |= pSPI_Handle->SPI_Config.SPI_CRCL << SPI_CR1_CRCL;
+
+	/* 4. Configure thhe clock polarity(idle state of the clock pulse) for SPI communication */
+	tempreg |= pSPI_Handle->SPI_Config.SPI_CPOL << SPI_CR1_CPOL;
+
+	/* 5. Configure thhe clock phase(data capture at which transition of clock pulse) for SPI communication */
+	tempreg |= pSPI_Handle->SPI_Config.SPI_CPHA << SPI_CR1_CPHA;
+
+	/* 6 Configure the baud rate(clock speed) for SPI transmission */
+	tempreg |= pSPI_Handle->SPI_Config.SPI_Speed << SPI_CR1_BAUDRATE;
+
+	/* 7. Configure whether the slave select management is done by hardware or software */
+	tempreg |= pSPI_Handle->SPI_Config.SPI_SSM << SPI_CR1_SSM;
+
+	/* copy the contents in tempreg variable to SPI_CR1 register */
+	pSPI_Handle->pSPIx->SPIx_CR1 = tempreg;
+}
 
 /*********************************************************************************************************************************
  *
