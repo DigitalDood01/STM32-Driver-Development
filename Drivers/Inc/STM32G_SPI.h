31096fc1054b3c4262a4befc3e6c87b595b46baa
Romanandhan <romanandhan@gmail.com>
2021-08-18 17:21:26 +0530

    Create macros for accessing the elements in SPI_Config structure

--- Drivers/Inc/STM32G_SPI.h
+++ Drivers/Inc/STM32G_SPI.h
@@ -14,13 +14,13 @@
 
 typedef struct
 {
-	uint8_t SPI_DeviceMode;
-	uint8_t SPI_BusConfig;
-	uint8_t SPI_DFF;
-	uint8_t SPI_CPHA;
-	uint8_t SPI_CPOL;
-	uint8_t SPI_SSM;
-	uint8_t SPI_Speed;
+	uint8_t SPI_DeviceMode;			/* Master or slave mode */
+	uint8_t SPI_BusConfig;			/* simplex/full-duplex/half-duplex */
+	uint8_t SPI_DFF;				/* data frame format 8-bit or 16-bit */
+	uint8_t SPI_CPHA;				/* clock phase */
+	uint8_t SPI_CPOL;				/* clock polarity */
+	uint8_t SPI_SSM;				/* software or hardware slave management */
+	uint8_t SPI_Speed;				/* Configuring the baud rate for transmission */
 
 }SPI_Config_t;
 
@@ -31,6 +31,49 @@ typedef struct
 
 }SPI_Handle_t;
 
+/******************************************* Macros for accessing the elements in SPI_Config_t structure**********************************************************/
+
+/* Macros for device mode configuration */
+
+#define SPI_DEVICE_MODE_MASTER 			1
+#define SPI_DEVICE_MODE_SLAVE 			0
+
+/* Macros for SPI bus configuration  */
+
+#define SPI_BUS_CONFIG_FD				1
+#define SPI_BUS_CONFIG_HD				2
+#define SPI_BUS_CONFIG_SIMPLEX_RXONLY	3
+
+/*Macros for configuring the serial clock speed */
+
+#define SPI_SCLK_SPEED_DIV2 			0
+#define SPI_SCLK_SPEED_DIV4 			1
+#define SPI_SCLK_SPEED_DIV8 			2
+#define SPI_SCLK_SPEED_DIV16 			3
+#define SPI_SCLK_SPEED_DIV32 			4
+#define SPI_SCLK_SPEED_DIV64 			5
+#define SPI_SCLK_SPEED_DIV128 			6
+#define SPI_SCLK_SPEED_DIV256 			7
+
+/* Macros for configuring the data frame format */
+
+#define SPI_DFF_8BITS 					0					/* DEFAULT MODE */
+#define SPI_DFF_16BITS 					1
+
+/* Macros for configuring the clock polarity */
+
+#define SPI_CPOL_HIGH 					1
+#define SPI_CPOL_LOW 					0
+
+/* Macros for configuring the clock phase */
+
+#define SPI_CPHA_HIGH 					1
+#define SPI_CPHA_LOW 					0
+
+/* Macros for configuring the SLAVE SELECT MANAGEMENT */
+
+#define SPI_SSM_ENABLE					0
+#define SPI_SSM_DISABLE					1
 
 /**********************************************************APIs Supported by this Driver***************************************************************************/
 
